07d55da2b039da48a87672c074bc97184f5c708a
Jiao Can <can.jiao@aitmed.com>
2022-01-05 09:13:18 +0800

    update

--- app/src/main/java/com/sena/dmzjthird/utils/UriUtil.java
+++ app/src/main/java/com/sena/dmzjthird/utils/UriUtil.java
@@ -1,11 +1,14 @@
 package com.sena.dmzjthird.utils;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.os.FileUtils;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
 
@@ -31,22 +34,63 @@ public class UriUtil {
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
 
-        }
+            if (DocumentsContract.isDocumentUri(context, uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                if ("com.ndroid.externalstorage.documents".equals(uri.getAuthority())) {
 
+                    String[] split = docId.split(":");
+                    String type = split[0];
+                    if ("primary".equalsIgnoreCase(type)) {
+                        return Environment.getExternalStorageDirectory() + "/" + split[1];
+                    }
+
+                } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+
+                    Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://download/public_downloads"), Long.valueOf(docId));
+                    return getPath(context, contentUri, null, null);
+
+                } else if ("com.android.providers.meida.documents".equals(uri.getAuthority())) {
+
+                    String[] split = docId.split(":");
+                    String type = split[0];
+                    Uri contentUri = null;
+                    if ("image".equals(type)) {
+                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("video".equals(type)) {
+                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("audio".equals(type)) {
+                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    }
+                    String selection = MediaStore.Images.Media._ID + "=?";
+                    String[] selectionArgs = new String[]{split[1]};
+                    return getPath(context, contentUri, selection, selectionArgs);
+
+                }
+
+            } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+                if ("com.google.android.apps.photos.content".equals(uri.getAuthority())) {
+                    return uri.getLastPathSegment();
+                }
+                return getPath(context, uri, null, null);
+
+            } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+                return uri.getPath();
+            }
+
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-
+            return getPath(context, uri);
         }
-
-
 
         return null;
     }
 
 
-    public static String getPath(Context context, Uri uri, String selection, String[] selectionArgs) {
+    private static String getPath(Context context, Uri uri, String selection, String[] selectionArgs) {
         String path = null;
-        Cursor cursor = null;
+        Cursor cursor;
         String column = MediaStore.Images.Media.DATA;
         String[] projection = {column};
         try {
@@ -64,7 +108,7 @@ public class UriUtil {
 
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
-    public static String getPath(Context context, Uri uri) {
+    private static String getPath(Context context, Uri uri) {
 
         File file = null;
         if (ContentResolver.SCHEME_FILE.equals(uri.getScheme())) {
