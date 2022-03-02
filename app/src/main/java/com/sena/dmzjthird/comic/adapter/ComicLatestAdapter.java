264a52679e394f0f96c97a19c42086b6d2f21372
Jiao Can <can.jiao@aitmed.com>
2022-03-02 18:44:36 +0800

    update

--- app/src/main/java/com/sena/dmzjthird/comic/adapter/ComicLatestAdapter.java
+++ app/src/main/java/com/sena/dmzjthird/comic/adapter/ComicLatestAdapter.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.module.LoadMoreModule;
 import com.chad.library.adapter.base.viewholder.BaseViewHolder;
+import com.example.application.ComicUpdateListRes;
 import com.sena.dmzjthird.R;
 import com.sena.dmzjthird.account.MyRetrofitService;
 import com.sena.dmzjthird.comic.bean.ComicLatestBean;
@@ -25,54 +26,54 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
  * Date: 2021/8/12
  * Time: 17:13
  */
-public class ComicLatestAdapter extends BaseQuickAdapter<ComicLatestBean, BaseViewHolder> implements LoadMoreModule {
+public class ComicLatestAdapter extends BaseQuickAdapter<ComicUpdateListRes.ComicUpdateListItemResponse, BaseViewHolder> implements LoadMoreModule {
 
     private final Context mContext;
 
     public ComicLatestAdapter(Context context) {
-        super(R.layout.item_comic_rank);
+        super(R.layout.item_object_latest);
         this.mContext = context;
     }
 
     @Override
-    protected void convert(@NonNull BaseViewHolder holder, ComicLatestBean bean) {
+    protected void convert(@NonNull BaseViewHolder holder, ComicUpdateListRes.ComicUpdateListItemResponse data) {
 
-        GlideUtil.loadImageWithCookie(mContext, "https://images.dmzj.com/"+bean.getCover(), holder.getView(R.id.cover));
+        GlideUtil.loadImageWithCookie(mContext, data.getCover(), holder.getView(R.id.cover));
 
-        holder.setText(R.id.title, bean.getName());
-        holder.setText(R.id.author, bean.getAuthors());
-        holder.setText(R.id.tag, bean.getTypes().replace("/", " "));
-        holder.setText(R.id.updateTime, TimeUtil.millConvertToDate(bean.getLast_updatetime()*1000));
-
-
-        holder.getView(R.id.subscribe).setOnClickListener(v -> {
+        holder.setText(R.id.title, data.getTitle());
+        holder.setText(R.id.author, data.getAuthors());
+        holder.setText(R.id.tag, data.getTypes().replace("/", " "));
+        holder.setText(R.id.latestChapter, data.getLastUpdateChapterName());
+        holder.setText(R.id.updateTime, TimeUtil.millConvertToDate(data.getLastUpdatetime() * 1000));
 
 
-            long uid = MyDataStore.getInstance(mContext).getValue(MyDataStore.DATA_STORE_USER, MyDataStore.USER_UID, 0L);
-            // 注释掉，可能增加一条无意义的请求
-            // 是否需要，后续在做讨论
-//            if (0L == uid) {
-//                Toast.makeText(mContext, mContext.getString(R.string.not_login), Toast.LENGTH_SHORT).show();
-//                return ;
-//            }
-            MyRetrofitService service = RetrofitHelper.getMyServer(MyRetrofitService.MY_BASE_URL);
-            service.controlSubscribe(uid, bean.getId(), bean.getCover(), bean.getName(), bean.getAuthors())
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(resultBean -> {
-                        if (resultBean == null) {
-                            XPopUpUtil.showCustomErrorToast(mContext, "请求失败，请稍后重试");
-                            return ;
-                        }
-                        if (resultBean.getCode() == 100) {
-                            XPopUpUtil.showCustomErrorToast(mContext, mContext.getString(R.string.not_login));
-                            return ;
-                        }
-                        holder.setBackgroundResource(R.id.subscribe,
-                                "true".equals(resultBean.getContent()) ? R.drawable.ic_subscribed_black : R.drawable.ic_subscribe_black);
-                    });
-
-        });
+//        holder.getView(R.id.subscribe).setOnClickListener(v -> {
+//
+//            long uid = MyDataStore.getInstance(mContext).getValue(MyDataStore.DATA_STORE_USER, MyDataStore.USER_UID, 0L);
+//            // 注释掉，可能增加一条无意义的请求
+//            // 是否需要，后续在做讨论
+////            if (0L == uid) {
+////                Toast.makeText(mContext, mContext.getString(R.string.not_login), Toast.LENGTH_SHORT).show();
+////                return ;
+////            }
+//            MyRetrofitService service = RetrofitHelper.getMyServer(MyRetrofitService.MY_BASE_URL);
+//            service.controlSubscribe(uid, bean.getId(), bean.getCover(), bean.getName(), bean.getAuthors())
+//                    .subscribeOn(Schedulers.io())
+//                    .observeOn(AndroidSchedulers.mainThread())
+//                    .subscribe(resultBean -> {
+//                        if (resultBean == null) {
+//                            XPopUpUtil.showCustomErrorToast(mContext, "请求失败，请稍后重试");
+//                            return ;
+//                        }
+//                        if (resultBean.getCode() == 100) {
+//                            XPopUpUtil.showCustomErrorToast(mContext, mContext.getString(R.string.not_login));
+//                            return ;
+//                        }
+//                        holder.setBackgroundResource(R.id.subscribe,
+//                                "true".equals(resultBean.getContent()) ? R.drawable.ic_subscribed_black : R.drawable.ic_subscribe_black);
+//                    });
+//
+//        });
 
     }
 }
--- app/src/main/java/com/sena/dmzjthird/comic/fragment/ComicLatestFragment.java
+++ app/src/main/java/com/sena/dmzjthird/comic/fragment/ComicLatestFragment.java
@@ -1,5 +1,6 @@
 package com.sena.dmzjthird.comic.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,14 +9,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.sena.dmzjthird.RetrofitService;
+import com.example.application.ComicUpdateListRes;
+import com.sena.dmzjthird.R;
 import com.sena.dmzjthird.comic.adapter.ComicLatestAdapter;
 import com.sena.dmzjthird.comic.bean.ComicLatestBean;
 import com.sena.dmzjthird.databinding.FragmentComicLatestBinding;
 import com.sena.dmzjthird.utils.IntentUtil;
-import com.sena.dmzjthird.utils.LogUtil;
-import com.sena.dmzjthird.utils.RetrofitHelper;
+import com.sena.dmzjthird.utils.api.ComicApi;
 
 import java.util.List;
 
@@ -24,29 +26,25 @@ import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
-import retrofit2.HttpException;
 
 public class ComicLatestFragment extends Fragment {
 
     private FragmentComicLatestBinding binding;
     private ComicLatestAdapter adapter;
-    private RetrofitService service;
-
-    private int page = 0;
 
     private boolean isLoaded = false;
+    private int page = 1;
+    private int mType = 100;
+
+    private TextView currentSelectTag;
 
     @Override
     public View onCreateView(@androidx.annotation.NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         binding = FragmentComicLatestBinding.inflate(inflater, container, false);
-        service = RetrofitHelper.getServer(RetrofitService.BASE_ORIGIN_URL);
 
-        initRecyclerView();
-        initRefreshLayout();
-
-        getResponse();
+        initView();
 
         return binding.getRoot();
     }
@@ -54,22 +52,42 @@ public class ComicLatestFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if (!isLoaded) {
-            isLoaded = true;
-            binding.refreshLayout.setRefreshing(true);
-            getResponse();
-        }
+        if (isLoaded) return ;
+        isLoaded = true;
+        lazyLoad();
     }
 
-    private void initRecyclerView() {
+    private void lazyLoad() {
+        binding.refreshLayout.setRefreshing(true);
+        getResponse();
+    }
+
+    private void initView() {
+
+        currentSelectTag = binding.selectAll;
+
+        initAdapter();
+
+        binding.refreshLayout.setOnRefreshListener(() -> {
+            page = 0;
+            getResponse();
+        });
+
+        binding.selectAll.setOnClickListener(v -> registerTagClick(binding.selectAll, 100));
+        binding.selectOriginal.setOnClickListener(v -> registerTagClick(binding.selectOriginal, 1));
+        binding.selectTranslate.setOnClickListener(v -> registerTagClick(binding.selectTranslate, 0));
+    }
+
+    private void initAdapter() {
         binding.recyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
         adapter = new ComicLatestAdapter(getActivity());
         binding.recyclerview.setAdapter(adapter);
-        adapter.setOnItemClickListener((adapter, view, position) -> {
+        adapter.setOnItemClickListener((a, view, position) -> {
 //            Intent intent = new Intent(getActivity(), ComicInfoActivity.class);
 //            intent.putExtra(getString(R.string.intent_comic_id), ((ComicLatestBean) adapter.getData().get(position)).getId());
 //            startActivity(intent);
-            IntentUtil.goToComicInfoActivity(getActivity(), ((ComicLatestBean) adapter.getData().get(position)).getId());
+
+            IntentUtil.goToComicInfoActivity(getActivity(), ((ComicLatestBean) a.getData().get(position)).getId());
         });
 
         adapter.getLoadMoreModule().setOnLoadMoreListener(this::getResponse);
@@ -77,56 +95,77 @@ public class ComicLatestFragment extends Fragment {
         adapter.getLoadMoreModule().setEnableLoadMoreIfNotFullPage(false);
     }
 
-    private void initRefreshLayout() {
-        binding.refreshLayout.setOnRefreshListener(() -> {
-            page = 0;
-            getResponse();
-        });
+    private void registerTagClick(TextView textView, int type) {
+        if (currentSelectTag == textView) return ;
+        currentSelectTag = textView;
+        mType = type;
+        page = 1;
+        if (currentSelectTag == binding.selectAll) {
+            binding.selectAll.setTextColor(getContext().getColor(R.color.theme_blue));
+            binding.selectAll.setBackgroundResource(R.drawable.shape_object_update_tag);
+        } else {
+            binding.selectAll.setTextColor(Color.BLACK);
+            binding.selectAll.setBackgroundColor(Color.WHITE);
+        }
+        if (currentSelectTag == binding.selectOriginal) {
+            binding.selectOriginal.setTextColor(getContext().getColor(R.color.theme_blue));
+            binding.selectOriginal.setBackgroundResource(R.drawable.shape_object_update_tag);
+        } else {
+            binding.selectOriginal.setTextColor(Color.BLACK);
+            binding.selectOriginal.setBackgroundColor(Color.WHITE);
+        }
+        if (currentSelectTag == binding.selectTranslate) {
+            binding.selectTranslate.setTextColor(getContext().getColor(R.color.theme_blue));
+            binding.selectTranslate.setBackgroundResource(R.drawable.shape_object_update_tag);
+        } else {
+            binding.selectTranslate.setTextColor(Color.BLACK);
+            binding.selectTranslate.setBackgroundColor(Color.WHITE);
+        }
+        initAdapter();
+        getResponse();
     }
 
     private void getResponse() {
-        service.getLatestComic(page)
+
+        ComicApi.getComicUpdate(mType, page)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<List<ComicLatestBean>>() {
+                .subscribe(new Observer<List<ComicUpdateListRes.ComicUpdateListItemResponse>>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
 
                     }
 
                     @Override
-                    public void onNext(@NonNull List<ComicLatestBean> beans) {
-                        if (beans.size() < 10) {
+                    public void onNext(@NonNull List<ComicUpdateListRes.ComicUpdateListItemResponse> dataList) {
+                        binding.refreshLayout.setRefreshing(false);
+                        if (page == 1 && dataList.size() == 0) {
+                            // 出错处理
+                            return ;
+                        }
+                        if (page == 1) {
+                            adapter.setList(dataList);
+                        } else {
+                            adapter.addData(dataList);
+                        }
+
+                        if (dataList.size() == 0) {
                             adapter.getLoadMoreModule().loadMoreEnd();
                         } else {
                             adapter.getLoadMoreModule().loadMoreComplete();
-                            page++;
                         }
-                        if (page == 0) {
-                            adapter.setList(beans);
-                        } else {
-                            adapter.addData(beans);
-                        }
+                        page++;
                     }
 
                     @Override
                     public void onError(@NonNull Throwable e) {
-                        if (binding.refreshLayout.isRefreshing()) {
-                            binding.refreshLayout.setRefreshing(false);
-                        }
-
-                        if (e instanceof HttpException) {
-                            LogUtil.e("HttpError: " + ((HttpException) e).code());
-                        } else {
-                            LogUtil.e("OtherError: " + e.getMessage());
-                        }
+                        binding.refreshLayout.setRefreshing(false);
+                        // 出错处理
                     }
 
                     @Override
                     public void onComplete() {
-                        if (binding.refreshLayout.isRefreshing()) {
-                            binding.refreshLayout.setRefreshing(false);
-                        }
+
                     }
                 });
     }
